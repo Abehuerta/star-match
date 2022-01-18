5dd74b6f75f37ca4b9634b5819b768ae5cd027f1
Abraham Huerta <abrhuerta@gmail.com>
2022-01-18 13:41:59 -0600

    Updates

--- src/components/StarDisplay/index.js
+++
@@ -1,1 +0,0 @@
-export { default as StarDisplay } from './StarDisplay';
--- src/theme/index.js
+++
@@ -1,7 +0,0 @@
-export const colors = {
-    background: '#175676',
-    available: '#c6c9fa',
-    used: 'lightgreen',
-    wrong: 'lightcoral',
-    candidate: '#6b70d6',
-};
